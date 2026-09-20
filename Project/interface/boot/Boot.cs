df97f36b768465a51fe8e9dc8f7a2cb4694aa8c5
Gdpro835 <209905124+Gdpro835@users.noreply.github.com>
2026-09-20 09:45:06 +0000

    Skip unstable Android boot videos
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- Project/interface/boot/Boot.cs
+++ Project/interface/boot/Boot.cs
@@ -5,17 +5,46 @@ namespace Project.Interface;
 
 public partial class Boot : Node
 {
-	public override void _Ready() => TransitionManager.Instance.LoadCommonResources();
+	private bool skipAndroidVideos;
+
+	public override void _Ready()
+	{
+		TransitionManager.Instance.LoadCommonResources();
+		skipAndroidVideos = OS.GetName().Equals("Android", System.StringComparison.OrdinalIgnoreCase);
+		if (skipAndroidVideos)
+		{
+			DisableVideo(GetNodeOrNull<VideoStreamPlayer>("Logo/SEGAVideo"));
+			DisableVideo(GetNodeOrNull<VideoStreamPlayer>("Logo/SFFVideo"));
+		}
+	}
 
 	[Export] private AnimationPlayer animator;
 
 	public override void _Process(double _delta)
 	{
+		if (skipAndroidVideos)
+		{
+			skipAndroidVideos = false;
+			animator.Stop();
+			StartTitleTransition();
+			return;
+		}
+
 		if (!animator.IsPlaying())
 			return;
 
 		if (Input.IsActionJustPressed("sys_select"))
 			AdvanceVideo();
+	}
+
+	private static void DisableVideo(VideoStreamPlayer player)
+	{
+		if (player == null)
+			return;
+
+		player.Stop();
+		player.Stream = null;
+		player.Visible = false;
 	}
 
 	private void StartTitleTransition()
